02b3933fb86dd6b40f048683f497e0283267cf24
Rhys Woolcott <rhyswoolcott@gmail.com>
2020-11-08 15:46:52 +0000

    Damn i hate assembly...

--- boot.asm
+++ boot.asm
@@ -4,65 +4,164 @@
 [org 0x7c00]
 [bits 16]
 
-section .data:
-    global main
+section .text
+
+  global main
 
 main:
-    ; hi
-cli
-jmp 0x000:ZSeg
-ZSeg:
-    mov si, loading_msg
-    call printf
+; Reset segment regs and set stack pointer to entry point
 
-    xor ax, ax
-    mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov sp, main
-    cld
+cli
+jmp 0x0000:ZeroSeg	; far jump to correct for BIOS putting us in the wrong segment
+ZeroSeg:
+  xor ax, ax
+  mov ss, ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov sp, main
+  cld
 sti
 
-push ax
-xor ax, ax
-int 0x13
-pop ax
+mov si, LOADING
+call printf
 
-mov al, 1
-mov cl, 2
+; Reset disk
+xor ax, ax
+mov bx, sTwo
+mov dl, 0x80
+int 0x13
+
+; Load sectors from our disk
+mov dl, 0x80
+mov al, 2		; sectors to read
+mov cl, 2		; start sector
+mov bx, sTwo		; offset
+; es is already 0
+call readDisk
+
+;mov ax, 0x2400
+;int 0x15
 
 call testA20
 mov dx, ax
 call printh
 
-mov dx, [0x7c00 + 510]
-call printh
+call enableA20
 
-call read_data
-jmp test
+jmp sTwo
 
-jmp $ ; Infinite loop
+jmp $ ;safety hang
 
 %include "./functions/printf.asm"
-%include "./functions/printh.asm"
 %include "./functions/read_data.asm"
+%include "./functions/printh.asm"
 %include "./functions/testA20.asm"
-%include "./functions/cls.asm"
-%include "./functions/delay.asm"
+%include "./functions/enableA20.asm"
 
-di_err_MSG: db "There was an error reading the disk. Please check your hardware", 0x0a, 0x0d, 0 ; Disk Error Message
-n_sect: db "Welcome To GalacOS Where Possibilities are ENDLESS. This operating system is far from complete so expect bugs around every corner.", 0x0a, 0x0d, 0 ; Disk Error Message
-loading_msg: db "Loading GalacOS", 0x0a, 0x0d, 0 ; Disk Error Message
+LOADING: db 'Loading...', 0x0a, 0x0d, 0
+DISK_ERR_MSG: db 'Error Loading Disk.', 0x0a, 0x0d, 0
+;TEST_STR: db 'You are in the second sector.', 0x0a, 0x0d, 0
+NO_A20: db 'No A20 line.', 0x0a, 0x0d, 0
+A20DONE: db 'A20 Line Enabled.', 0x0a, 0x0d, 0
+NO_LM: db 'No long mode support.', 0x0a, 0x0d, 0
+YES_LM: db 'Long Mode supported.', 0x0a, 0x0d, 0
 
-times 510-($-$$) db 0 ; Padding
-dw 0xaa55 ; Magic Number
+; padding and magic number
+times 510-($-$$) db 0
+dw 0xaa55
 
-test:
-call delay
-call cls
-mov si, n_sect
+sTwo:
+mov si, TEST_STR
 call printf
 
-times 512 db 0
+call checklm
+
+push bx
+xor bx, bx
+mov es, bx
+pop bx
+
+mov dl, 0x80
+mov al, 3
+mov cl, 3
+mov bx, Kernel
+call readDisk
+
+cli
+
+mov edi, 0x1000
+mov cr3, edi
+xor eax, eax
+mov ecx, 4096
+rep stosd
+mov edi, 0x1000
+
+;PML4T -> 0x1000
+;PDPT -> 0x2000
+;PDT -> 0x3000
+;PT -> 0x4000
+
+mov dword [edi], 0x2003
+add edi, 0x1000
+mov dword [edi], 0x3003
+add edi, 0x1000
+mov dword [edi], 0x4003
+add edi, 0x1000
+
+mov dword ebx, 3
+mov ecx, 512
+
+.setEntry:
+  mov dword [edi], ebx
+  add ebx, 0x1000
+  add edi, 8
+  loop .setEntry
+
+mov eax, cr4
+or eax, 1 << 5
+mov cr4, eax
+
+mov ecx, 0xc0000080
+rdmsr
+or eax, 1 << 8
+wrmsr
+
+mov eax, cr0
+or eax, 1 << 31
+or eax, 1 << 0
+mov cr0, eax
+
+lgdt [GDT.Pointer]
+jmp GDT.Code:LongMode
+
+TEST_STR: db 'You are in the second sector.', 0x0a, 0x0d, 0
+
+%include "./functions/checklm.asm"
+%include "./functions/gdt.asm"
+
+[bits 64]
+LongMode:
+
+VID_MEM equ 0xb8000
+mov edi, VID_MEM
+mov rax, 0x8f208f208f208f20
+mov ecx, 500
+rep stosq
+
+jmp Kernel
+
+hlt
+times 512-($-$$-512) db 0
+;times 1048576 - ($-($$ + 512)) db 0 ; precise padding to fill up 1MB
+
+Kernel:
+
+mov rax, 0x1f6f1f6c1f6c1f651f48
+mov [VID_MEM], rax
+
+hlt
+
+times 4096 db 0
+;times 5120 db 0
